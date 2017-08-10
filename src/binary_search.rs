4350eb95f378fc2e053ce055b52d5878164fecb0
Arjun Mannaly <a.mannaly@gmail.com>
2017-08-09 20:58:23 -0700

    correct indentation

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -1,22 +1,21 @@
 pub fn binary_search(array: &[i32], target: i32) -> Option<usize> {
 
-	let mut start = 0;
-	let mut end = array.len() - 1;
+    let mut start = 0;
+    let mut end = array.len() - 1;
 
-	while start <= end {
-		
-		let mid = start + (end-start)/2;
+    while start <= end {
+        let mid = start + (end-start)/2;
 
-		if array[mid] == target {
-			return Some(mid);
-		}
-		else if array[mid] < target {
-			start = mid + 1;
-		}
-		else {
-			end = mid - 1;
-		}
-	}
+        if array[mid] == target {
+            return Some(mid);
+        }
+        else if array[mid] < target {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
 
-	None
+    None
 }
--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -1,37 +1,37 @@
 pub fn sort(array: &mut [i32]) {
-	let length = array.len()-1;
-	_sort(array, 0, length);
+    let length = array.len()-1;
+    _sort(array, 0, length);
 }
 
 fn _sort(array: &mut [i32], start: usize, end: usize) {
 
-	if start < end {
+    if start < end {
 
-		let p =  partition(array, start, end);
-		_sort(array, start, p-1);
-		_sort(array, p+1, end);
-	}
+        let p =  partition(array, start, end);
+        _sort(array, start, p-1);
+        _sort(array, p+1, end);
+    }
 }
 
 fn partition(array: &mut [i32], start: usize, end: usize) -> usize {
 
-	let pivot = array[end];
-	let mut p = end;
-	for i in (start..end).rev() {
+    let pivot = array[end];
+    let mut p = end;
+    for i in (start..end).rev() {
 
-		if array[i] >= pivot {
-			p = p-1;
-			swap(array, i, p);			
-		}
-	}
+        if array[i] >= pivot {
+            p = p-1;
+            swap(array, i, p);          
+        }
+    }
 
-	swap(array, end, p);			
+    swap(array, end, p);            
 
-	p
+    p
 }
 
 fn swap(array: &mut [i32], source: usize, dest: usize) {
-	let temp = array[dest];
-	array[dest] = array[source];
-	array[source] = temp;
+    let temp = array[dest];
+    array[dest] = array[source];
+    array[source] = temp;
 }
